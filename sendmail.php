044b99e49477f9fa15f5310db44311996af009e0
florian-priegnitz <128146061+florian-priegnitz@users.noreply.github.com>
2023-04-21 00:27:30 +0200

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -8,6 +8,7 @@ $subject = "Kontaktformular Nachricht";
 // Sammeln Sie die Informationen aus dem Formular
 $name = $_POST["name"];
 $email = $_POST["email"];
+$phone = $_POST["phone"]; // Fügen Sie die Telefonnummer hinzu
 $message = $_POST["message"];
 $captcha = $_POST["captcha"];
 $num1 = $_POST["num1"];
@@ -22,14 +23,21 @@ if ($captcha != $correct_answer) {
 
 // Erstellen Sie den E-Mail-Text
 $email_body = "Name: $name\n";
-$email_body .= "E-Mail: $email\n\n";
+$email_body .= "E-Mail: $email\n";
+$email_body .= "Telefon: $phone\n\n"; // Fügen Sie die Telefonnummer zum E-Mail-Text hinzu
 $email_body .= "Nachricht:\n$message\n";
-
 
 // Setzen Sie den "From" Header für die E-Mail
 $headers = "From: $email";
 
 // Senden Sie die E-Mail
 mail($to, $subject, $email_body, $headers);
-header("Location: index.html"); // Leiten Sie den Benutzer zurück zur Hauptseite
+
+// Senden Sie dem Benutzer eine Kopie der E-Mail
+$user_subject = "Kopie Ihrer Kontaktformular Nachricht";
+$user_message = "Vielen Dank f&uuml;r Ihre Nachricht. Hier ist eine Kopie Ihrer E-Mail:\n\n";
+$user_message .= $email_body;
+mail($email, $user_subject, $user_message, "From: $to"); // Senden Sie die Kopie an den Benutzer
+
+header("Location: danke.html"); // Leiten Sie den Benutzer zurück zur Hauptseite
 ?>
